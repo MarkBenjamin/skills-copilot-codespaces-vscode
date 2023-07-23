b521ecf689496489d1357a70827fe8e06987f6e4
MarkBenjamin <136419977+MarkBenjamin@users.noreply.github.com>
2023-07-23 17:14:22 +0000

    added skills function

---
+++ member.js
@@ -0,0 +1,9 @@
+function skillsMember() {
+    var skills = ["HTML", "CSS", "JS", "PHP", "MySQL"];
+    var member = {
+        name: "David",
+        age: 25,
+        skills: skills
+    };
+    return member.skills;
+}
